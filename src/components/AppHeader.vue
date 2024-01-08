10b43b9e5eab3802cb0964e0daf39d44695f578a
federicaarosio <arosio.fede@gmail.com>
2024-01-08 11:58:38 +0100

    working on AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,36 @@
 <template lang="">
-    <h2>
-        AppHeader
-    </h2>
-    <i class="fa-brands fa-facebook"></i>
-    <i class="fa-solid fa-house"></i>
+    <section class="header">
+        <section class="info-contacts">
+            <div>
+                North
+            </div>
+            <div>
+                orario
+            </div>
+        </section>
+    
+        <section class="navbar">
+    
+        </section>
+    </section>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+    section.header {
+        color: $white;
+    }
+
+    section.info-contacts {
+        @include flex(row, space-between, center);
+        background-color: $main-color;
+        height: 20px;
+    }
     
 </style>
