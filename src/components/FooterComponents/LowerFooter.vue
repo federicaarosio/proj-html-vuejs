2f7536f19352388297e38b1008aaad22c2158116
federicaarosio <arosio.fede@gmail.com>
2024-01-09 15:31:29 +0100

    lower footer completed

--- src/components/FooterComponents/LowerFooter.vue
+++ src/components/FooterComponents/LowerFooter.vue
@@ -1,13 +1,49 @@
 <template lang="">
-    <div>
-        Lower
-    </div>
+    <section class="lower-footer">
+        <div class="container">
+            <div>
+                Copyright &#169; gogrin all rights reserved.
+            </div>
+            <div class="links-container">
+                <div>
+                    Service
+                </div>
+                <div>
+                    Pricing Table
+                </div>
+                <div>
+                    Contact
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
 export default {
     name: "LowerFooter",
 }
 </script>
-<style lang="">
+<style lang="scss" scoped>
+@use "../../styles/partials/mixins" as *;
+@use "../../styles/partials/variables" as *;
+
+section.lower-footer {
+    background-color: $main-color;
+    color: $white;
+    font-size: $font-small;
+
+    div.container {
+        width: $small-container;
+        margin: 0 auto;
+        padding-top: $padding-small;
+        padding-bottom: $padding-small;
+        @include flex(row, space-between, center);
+
+        .links-container {
+            @include flex(row, space-between, center);
+            gap: 20px;
+        }
+    }
+}
     
 </style>
