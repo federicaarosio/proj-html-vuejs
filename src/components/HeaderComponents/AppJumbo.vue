561fb85db6dc95aa5c2c0a3e4b3ce02d9b1e3e98
federicaarosio <arosio.fede@gmail.com>
2024-01-09 16:00:28 +0100

    working on jumbo slider

--- src/components/HeaderComponents/AppJumbo.vue
+++ src/components/HeaderComponents/AppJumbo.vue
@@ -1,12 +1,40 @@
 <template lang="">
     <section class="jumbo">
         <img src="../../assets/img/H3-slider3.jpg">
+
+        <SingleSliderimg v-for="(slide, index) in sliderImages"
+        :index ="index"
+        :visibleSlide="visibleSlide"
+        :src="`../../public/img/${slide.src}.jpg`"/>
+
     </section>
 </template>
 
 <script>
+import SingleSliderimg from './SingleSliderimg.vue';
+
 export default {
     name: "AppJumbo",
+
+    components: {
+        SingleSliderimg
+    },
+
+    data() {
+        return {
+            sliderImages: [
+                {
+                    src: "H3-slider3",
+                },
+                {
+                    src: "slider",
+                },
+
+            ],
+
+            visibleSlide: 0,
+        }
+    },
 }
 </script>
 
---
+++ src/components/HeaderComponents/SingleSliderimg.vue
@@ -0,0 +1,34 @@
+<template lang="">
+    <div>
+        S L I D E R
+    </div>
+    <div class="container" v-show="visibleSlide === index" >
+        <img :src="src">
+    </div>
+</template>
+<script>
+export default {
+    name: "SingleSliderimg",
+
+    props: {
+        visibleSlide: {
+            type: Number,
+            required: true
+        },
+
+        index: {
+            type: Number,
+            required: true
+        },
+
+        src: {
+            type: String,
+            requierd: true
+        }
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
