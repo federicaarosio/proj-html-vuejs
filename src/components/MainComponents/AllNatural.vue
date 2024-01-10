9197a4df2638d894409b8fca893de8d1dc1bbbc5
federicaarosio <arosio.fede@gmail.com>
2024-01-10 16:08:30 +0100

    working on slider

--- src/components/MainComponents/AllNatural.vue
+++ src/components/MainComponents/AllNatural.vue
@@ -14,18 +14,98 @@
                 <img src="../../assets/img/fe-shap1-1.png" alt="">
             </div>
         </section>
-        <section class="container">
+
+        <section class="container-slider container">
+            <MicroSlider v-for="(slide, index) in visibleImages"
+            :index ="index"
+            :visibleSlide="visibleSlide"
+            :src="`../../img/${slide.src}.jpg`"
+            />
+            <button class="prev" @click="prevSlide">
+                <i class="fa-solid fa-chevron-left"></i>
+            </button>
+    
+            <button class="next" @click="nextSlide">
+                <i class="fa-solid fa-chevron-right"></i>
+            </button>
+        </section>
+
+        
+        <!-- <section class="container">
             <img src="../../assets/img/fe3.jpg">
             <img src="../../assets/img/fe1.jpg">
             <img src="../../assets/img/fe2.jpg">
             <img src="../../assets/img/fe1.jpg">
-        </section>
+        </section> -->
     </section>
 </template>
 
 
 <script>
+import MicroSlider from '../MainComponents/MicroSlider.vue';
 export default {
+    components: {
+        MicroSlider,
+    },
+
+    data() {
+        return {
+            sliderImages: [
+                {
+                    src: "fe3",
+                },
+                {
+                    src: "fe2",
+                },
+                {
+                    src: "fe1",
+                },
+                {
+                    src: "fe2",
+                },
+                {
+                    src: "fe3",
+                },
+                {
+                    src: "fe2",
+                },
+                {
+                    src: "fe1",
+                },
+                {
+                    src: "fe2",
+                },
+            ],
+
+            visibleSlide: 0,
+
+            itemsPerPage: 4,
+        }
+    },
+
+    methods: {
+        nextSlide() {
+            if (this.visibleSlide >= this.sliderImages.length - 1) {
+                this.visibleSlide = 0;
+            } else {
+                this.visibleSlide ++;
+            };
+        },
+
+        prevSlide() {
+            if (this.visibleSlide <= 0) {
+                this.visibleSlide = this.sliderImages.length - 1;
+            } else {
+                this.visibleSlide --;
+            };
+        }
+    },
+
+    computed: {
+    visibleImages() {
+      return this.sliderImages.slice(this.visibleSlide, this.visibleSlide + this.itemsPerPage);
+    },
+  },
 }
 </script>
 
@@ -43,9 +123,22 @@ section.container {
     @include flex(row, space-between, start);
     gap: 20px;
     margin-bottom: $margin-bottom-small;
+    position: relative;
 
-    img {
-        width: calc((100% / 4) - 20px);
+    button {
+        position: absolute;
+    }
+
+    button.prev {
+        top: 50%;
+        left: -10px;
+        transform: translateY(-50%);
+    }
+
+    button.next {
+        top: 50%;
+        right: -10px;
+        transform: translateY(-50%);
     }
 
     div.sub-container {
@@ -75,8 +168,6 @@ section.container {
         font-size: $font-big;
         font-weight: 800;
         color: $main-color;
-
-
     }
 }
 
---
+++ src/components/MainComponents/MicroSlider.vue
@@ -0,0 +1,43 @@
+<template lang="">
+    <!-- <div class="container" v-show="visibleSlide === index"  >
+        <img :src="src">
+    </div> -->
+    <div class="container">
+        <img :src="src">
+    </div>     
+</template>
+<script>
+export default {
+    name: "MicroSlider",
+    props: {
+        visibleSlide: {
+            type: Number,
+            required: true
+        },
+
+        index: {
+            type: Number,
+            required: true
+        },
+
+        src: {
+            type: String,
+            requierd: true
+        }
+    }
+    
+}
+</script>
+<style lang="scss" scoped>
+@use "../../styles/partials/mixins" as *;
+@use "../../styles/partials/variables" as *;
+
+div.container {
+        width: calc(100% / 4);
+
+        img {
+        width: 100%;
+    }
+    }
+    
+</style>
